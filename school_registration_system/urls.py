f15b864a8f6a2c19a8f951ecdb37b9081d1da978
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-05-06 15:20:01 +0530

    Test

--- school_registration_system/urls.py
+++ school_registration_system/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from drf_yasg import openapi
-from django.views.generic import TemplateView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 
@@ -10,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('schoolApp.urls')),
     path('api/',include('school_api.urls')),
-    path('customadmin/',include("custom_admin.urls.urls_auth"))
+    # path('customadmin/',include("custom_admin.urls")),
 ]
 
 schema_view = get_schema_view(
