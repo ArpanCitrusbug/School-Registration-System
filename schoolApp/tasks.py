a1cda2ee5dc505eb7fa8fc7bcd3316d3fa8ba159
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-05-17 17:13:12 +0530

    improved

--- schoolApp/tasks.py
+++ schoolApp/tasks.py
@@ -9,8 +9,8 @@ def mail_send_function():
     print("Hello###########################################################")
     teacher = Teacher.objects.filter(is_student=False)
     for i in teacher:
-        mail_subject = "Hi!!!! We are testing Celery"
-        meassage = "This is Celery Testing. Only Printed mails are send here."
+        mail_subject = f"Login User Accessed Detailed View of {teacher.username}"
+        meassage = "You Opened the Detailed page of Teacher's Login"
         to_email = i.email
         send_mail(
             subject= mail_subject,
@@ -20,3 +20,38 @@ def mail_send_function():
             fail_silently= True,
         )
     return "Done"
+
+
+
+
+
+
+
+
+
+import sendgrid
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from event_module.settings import SENDGRID_API_KEY
+
+@shared_task(bind=True)
+def sendgrid_mail(self, serializer):
+    message = mail(from_email="jayendra.citrusbug@gmail.com",
+                    to_emails="jayendraiant@gmail.com",
+                    subject="Sending with SendGrid is fun...!",
+                    plain_text_content="and easy to do anywhere, even with Python.")
+    message.dynamic_template_data={"organiser_name":serializer[0].get('organiser_name'),
+                                    "event_name":serializer[0].get('event_name'),
+                                    "event_address":serializer[0].get('event_address'),
+                                    "dates":serializer[1],
+                                    "access_points":serializer[2]}
+    message.template_id='d-a0df076a5ece4aa0849c8155d3bd9b7f'
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
